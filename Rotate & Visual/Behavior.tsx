156dc90a2f3246587a9b9d4c4afc965e82da754c
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-08-29 20:24:57 -0300

    Create Behavior.tsx

---
+++ Rotate & Visual/Behavior.tsx
@@ -0,0 +1,12 @@
+import 'react'
+
+const haviour = <h1>Is Haviour!</h1>
+
+let exportBeHaviour = haviour
+
+var haviourDefine = 0
+
+// Set to 0.
+if (haviourDefine = 2) {
+  const done = <h1>Haviour is done!</h1>
+}
